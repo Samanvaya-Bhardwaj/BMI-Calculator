055aa745d62d3cd0670e97ca50bc93ffe8664de5
Samanvaya Bhardwaj <samanvayabhardwaj2001@gmail.com>
2022-07-09 18:27:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react'
+
+import './index.css'
+
+function App() {
+
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+
+
+  let calcBmi = (event) => {
+    event.preventDefault()
+
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height))
+      setBmi(bmi.toFixed(1))
+
+      if (bmi < 18) {
+        setMessage('You are underweight')
+      } else if (bmi >= 18 && bmi < 25) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+
+
+  if (bmi < 1) {
+    console.log('No Comments')
+  } else {
+    if(bmi < 25) {
+      console.log('Under weight'+bmi)
+    } else if (bmi >= 25 && bmi < 30) {
+      console.log('Health weight' + bmi)
+    } else {
+      console.log('Over Weight' + bmi)
+    }
+  }
+
+
+  let reload = () => {
+    window.location.reload()
+  }
+
+  return (
+    <div className="app">
+      <div className='container'>
+        <h2 className='center'>BMI Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (kg)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (metres)</label>
+            <input value={height} onChange={(event) => setHeight(event.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Calculate</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Clear</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
